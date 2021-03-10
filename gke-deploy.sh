d252d4d8902dbbf19fd250bbff485c8cbcc79caf
Cody Coggins <36673549+codycoggins-virtru@users.noreply.github.com>
2021-03-09 23:42:58 -0500

    Fix schema.yaml - "No properties should have x-google-marketplace.type=IMAGE"

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -3,6 +3,13 @@
 # pwd should be /your/path/to/virtru-public
 # Reference: https://docs-development.preprod.virtru.com/gcp/gcr/
 
+# Prerequisite: install Application CRD?
+# kubectl apply -f "https://raw.githubusercontent.com/GoogleCloudPlatform/marketplace-k8s-app-tools/master/crd/app-crd.yaml"
+
+# Prerequisite: connect to GKE if you want to deploy there.
+# gcloud container clusters get-credentials marketplace-gateway --region us-central-1 --region us-central1-a
+
+
 # Authenticate gcloud with your Google account:
 #   gcloud auth login
 # (Follow login prompts in the web browser)
