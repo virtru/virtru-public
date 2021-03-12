ef354a8ef82a4e496506124a56212053ea966696
Cody Coggins <36673549+codycoggins-virtru@users.noreply.github.com>
2021-03-12 11:00:16 -0500

    Cleanup

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Deploy to GKE staging environment using mpdev
+# This script illustrates how to deploy to GKE staging environment using mpdev
 # pwd should be /your/path/to/virtru-public
 # Reference: https://docs-development.preprod.virtru.com/gcp/gcr/
 
@@ -9,12 +9,12 @@
 # Prerequisite: connect to GKE if you want to deploy there.
 # gcloud container clusters get-credentials marketplace-gateway --region us-central-1 --region us-central1-a
 
-
 # Authenticate gcloud with your Google account:
 #   gcloud auth login
 # (Follow login prompts in the web browser)
 # Then apply Google auth to docker:
 #   gcloud auth configure-docker
+
 set -eu
 
 export TAG=2.16.1;
@@ -27,5 +27,3 @@ docker push "${REGISTRY}/deployer:${DEPLOYER_VERSION}"
 # mpdev install to install, mpdev verify to test
 mpdev install --deployer="${REGISTRY}/deployer:${DEPLOYER_VERSION}" \
 --parameters='{"name": "gateway", "namespace": "virtru", "gatewayHostname": "gateway-development.virtru.com", "gatewayApiTokenName": "token", "gatewayApiSecret": "mysecret", "licensedUsers":"10", "primaryMailingDomain":"example.com", "reportingSecret":"gs://cloud-marketplace-tools/reporting_secrets/fake_reporting_secret.yaml"}'
-
-#kubectl logs -f job/gateway-deployer
