00ec92c53cfdd27766de7f15375338fbf85da200
Cody Coggins <36673549+codycoggins-virtru@users.noreply.github.com>
2021-04-01 18:01:07 -0400

    Use hourly fractional billing to better fit Google's model.

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -19,11 +19,11 @@ set -eu
 
 export TAG=2.15.2;
 export DEPLOYER_VERSION=2.15;
-export REGISTRY=gcr.io/virtru-public/gateway;
+export REGISTRY=gcr.io/virtru-public/staging/gateway;
 docker build --no-cache --build-arg TAG=$TAG --build-arg REGISTRY=$REGISTRY \
   -t "${REGISTRY}/deployer:${DEPLOYER_VERSION}" -f dev.Dockerfile .
 docker push "${REGISTRY}/deployer:${DEPLOYER_VERSION}"
 
 # mpdev install to install, mpdev verify to test
 mpdev install --deployer="${REGISTRY}/deployer:${DEPLOYER_VERSION}" \
---parameters='{"name": "gateway", "namespace": "virtru", "gatewayHostname": "gateway-development.virtru.com", "gatewayApiTokenName": "token", "gatewayApiSecret": "mysecret", "numberOfLicenses":"10", "primaryMailingDomain":"virtru.example.com", "reportingSecret":"gateway-reportingsecret"}'
+--parameters='{"name": "gateway", "namespace": "virtru", "gatewayHostname": "gateway-development.virtru.com", "gatewayApiTokenName": "token", "gatewayApiSecret": "mysecret", "numberOfLicenses":"10", "primaryMailingDomain":"virtru.example.com", "reportingSecret":"gs://cloud-marketplace-tools/reporting_secrets/fake_reporting_secret.yaml"}'
