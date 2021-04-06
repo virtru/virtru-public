5484cb7c419b2a594ffffd9a927500250823a34e
Cody Coggins <36673549+codycoggins-virtru@users.noreply.github.com>
2021-04-06 14:54:04 -0400

    2.15.2 Submission - Billing using User-days, reported in fractions each hour.

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -19,7 +19,7 @@ set -eu
 
 export TAG=2.15.2;
 export DEPLOYER_VERSION=2.15;
-export REGISTRY=gcr.io/virtru-public/staging/gateway;
+export REGISTRY=gcr.io/virtru-public/gateway;
 docker build --no-cache --build-arg TAG=$TAG --build-arg REGISTRY=$REGISTRY \
   -t "${REGISTRY}/deployer:${DEPLOYER_VERSION}" -f dev.Dockerfile .
 docker push "${REGISTRY}/deployer:${DEPLOYER_VERSION}"
